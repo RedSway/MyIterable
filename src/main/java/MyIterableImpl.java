2256e2ae213c9e797217547f2adab8d1f1aaa17d
RedSway <36565752+RedSway@users.noreply.github.com>
2019-09-09 17:09:07 +0300

    Update MyIterableImpl.java

--- src/main/java/MyIterableImpl.java
+++ src/main/java/MyIterableImpl.java
@@ -3,7 +3,9 @@ import javax.annotation.Nullable;
 import java.util.*;
 
 public class MyIterableImpl implements MyIterable<Integer> {
-    List<Integer> list;
+    private List<Integer> list;
+    private Predicate predicate;
+    private Function transform;
     private static MyIterableImpl myIterable = null;
 
     private MyIterableImpl() {
@@ -29,26 +31,39 @@ public class MyIterableImpl implements MyIterable<Integer> {
 
     @Override
     public MyIterable<Integer> filter(@Nonnull Predicate<Integer> predicate) {
-        List<Integer> tmpList = new ArrayList<Integer>();
-        for (Integer integer : list) {
-            if (predicate.apply(integer))
-                tmpList.add(integer);
+        if (getInstance().predicate == null) {
+            this.predicate = predicate;
+        } else {
+            List<Integer> tmpList = new ArrayList<Integer>();
+
+            for (Integer integer : getInstance().list) {
+                if (predicate.apply(integer))
+                    tmpList.add(integer);
+            }
+
+            getInstance().list = new ArrayList<Integer>(tmpList);
         }
-        list = new ArrayList<Integer>(tmpList);
+
         return getInstance();
     }
 
     @Override
     public <T> MyIterable<T> transform(@Nonnull Function<Integer, T> transformer) {
-        for (int i = 0; i < list.size(); i++) {
-            list.set(i, (Integer) transformer.apply(list.get(i)));
+        if (getInstance().transform == null) {
+            this.transform = transformer;
+        } else {
+            for (int i = 0; i < getInstance().list.size(); i++) {
+                getInstance().list.set(i, (Integer) transformer.apply(getInstance().list.get(i)));
+            }
         }
-
         return (MyIterable<T>) getInstance();
     }
 
     @Override
     public <T> T aggregate(@Nullable T initValue, @Nonnull Aggregator<Integer, T> aggregator) {
+        getInstance()
+                .filter(predicate)
+                .transform(transform);
         Integer result = (Integer) initValue;
         for (Integer integer : list) {
             result = (Integer) aggregator.apply((T) result, integer);
@@ -69,15 +84,22 @@ public class MyIterableImpl implements MyIterable<Integer> {
     @Nullable
     @Override
     public Integer findFirst(@Nonnull Predicate<Integer> predicate) {
-        for (Integer integer : list) {
-            if (predicate.apply(integer))
-                return integer;
+        Iterator<Integer> iterator = getInstance().iterator();
+
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (this.predicate.apply(next)) {
+                next = (Integer) this.transform.apply(next);
+                if (predicate.apply(next))
+                    return next;
+            }
+
         }
         return 0;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return list.iterator();
     }
 }
